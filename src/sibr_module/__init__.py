48b76d12ab6e8916e97465e7096b509de85a837b
sigvardbratlie <sigvard.bratlie@gmail.com>
2025-09-15 09:04:21 +0200

    added LoggerV2 module

--- src/sibr_module/__init__.py
+++ src/sibr_module/__init__.py
@@ -1,8 +1,9 @@
-from .google_helpers import BigQuery, Logger, SecretsManager, CStorage
+from .google_helpers import BigQuery, Logger, SecretsManager, CStorage,LoggerV2
 
 __all__ = [
     "BigQuery",
     "Logger",
+    "LoggerV2",
     "SecretsManager",
     "CStorage"
 ]
